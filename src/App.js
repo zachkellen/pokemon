157e301fc22b4823b264364ad73052646eb3f43c
Zach Kellen <z.kellen12@gmail.com>
2021-09-07 14:24:41 -0500

    Pokemon API working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [pokemon, setPokemon] = useState([]);
+
+  const fetchAPI = (event) => {
+    event.preventDefault();
+    fetch('https://pokeapi.co/api/v2/pokemon')
+      .then(res => res.json())
+      .then(res => {
+        console.log(res.results[0].name)
+        setPokemon(res.results)
+      })
+      .catch(err => console.log(err));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Here are the Pokemen</h1>
+      <button onClick={fetchAPI}>Get my Pokes</button>
+      <br></br>
+      <ul>
+        {
+          pokemon.map((item,i)=>{
+            return <li key={i}>{item.name}</li>
+          })
+        }
+      </ul>
     </div>
   );
 }
